576216029b55db44b2b07ef2ecee813a39dec291
dkStephanos <stephanos@goldmail.etsu.edu>
2020-11-04 17:17:19 -0500

    Added params to server node for IP

--- LabWeek11App/ServerNode.cs
+++ LabWeek11App/ServerNode.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Linq;
+using System.Net;
 
 namespace LabWeek11App
 {
    public class ServerNode : IObservable<string>
    {
       private readonly ConcurrentBag<IObserver<string>> _observers;
+      int PortNumber { get; set; }
+      IPAddress IPAddress { get; set; }
+      private SocketAddress _listener { get; set; }
+      private IPEndPoint _localEndPoint { get; set; }
 
-      public ServerNode()
+
+
+      public ServerNode(int portNumber)
       {
          _observers = new ConcurrentBag<IObserver<string>>();
+         PortNumber = portNumber;
       }
 
       public IDisposable Subscribe(IObserver<string> observer)
