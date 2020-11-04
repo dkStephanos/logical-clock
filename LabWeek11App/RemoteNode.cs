ea7ffbfacbbe32e572eb0d99eb740996e262bc19
dkStephanos <stephanos@goldmail.etsu.edu>
2020-11-04 17:53:42 -0500

    Adding class RemoteNode

---
+++ LabWeek11App/RemoteNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace LabWeek11App
+{
+   class RemoteNode
+   {
+      private Socket _remoteSocket;
+      public IPEndPoint RemoteEndPoint { get; set; }
+      public ServerNode LocalServer { get; set; }
+
+      public RemoteNode(ServerNode localServer)
+      {
+         LocalServer = localServer;
+         _remoteSocket = null;
+         RemoteEndPoint = null;
+      }
+   }
+}
