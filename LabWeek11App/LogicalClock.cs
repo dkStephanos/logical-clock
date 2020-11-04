c90b9623ddf6604af682f33f247242e858663af1
dkStephanos <stephanos@goldmail.etsu.edu>
2020-11-04 18:17:11 -0500

    Set up LogicalClock class

---
+++ LabWeek11App/LogicalClock.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+
+namespace LabWeek11App
+{
+   class LogicalClock : IDisposable
+   {
+      private System.Timers.Timer _timer;
+      private int _interval;
+      public long Counter { get; set; }
+      public int Step { get; set; }
+
+      public LogicalClock(int interval, int step)
+      {
+         _interval = interval;
+         Step = step;
+
+         _timer = new System.Timers.Timer(_interval);
+         _timer.AutoReset = true;
+         _timer.Elapsed += OnTick;
+      }
+
+      public void OnTick(object sender, ElapsedEventArgs e)
+      {
+         Counter += Step;
+      }
+
+      public void Start()
+      {
+         _timer.Start();
+      }
+
+      public void Stop()
+      {
+         _timer.Stop();
+      }
+
+      public void Dispose()
+      {
+         Stop();
+         _timer.Dispose();
+      }
+   }
+}
